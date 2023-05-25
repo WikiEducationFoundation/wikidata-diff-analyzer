c52bbb5b70d12315db3d9290dbd76e824f67a3bc
saha23s <saha23s2mtholyoke.edu>
2023-05-25 09:32:45 +0600

    get_revision_item_json and test cases

--- lib/wikidata/diff/analyzer.rb
+++ lib/wikidata/diff/analyzer.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
 require_relative "analyzer/version"
+require 'json'
+require 'open-uri'
 
 module Wikidata
   module Diff
     module Analyzer
       class Error < StandardError; end
-      # Your code goes here...
+      def self.get_revision_item_json(item_id, revision_id)
+        url = "https://www.wikidata.org/w/api.php?action=query&prop=revisions&titles=#{item_id}&rvslots=main&rvprop=content&format=json&rvstartid=#{revision_id}"
+        response = URI.open(url).read
+        json = JSON.parse(response)
+        page_id = json['query']['pages'].keys.first
+        return json['query']['pages'][page_id]['revisions'][0]
+      end
     end
   end
 end
