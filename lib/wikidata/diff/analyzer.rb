103d71aea75bcd40256c878cc82edf9f6917e76a
Sulagna Saha <saha23s@mtholyoke.edu>
2023-05-29 09:15:26 +0600

    claims and reference counts with test cases

--- lib/wikidata/diff/analyzer.rb
+++ lib/wikidata/diff/analyzer.rb
@@ -24,9 +24,53 @@ module Wikidata
         page_id = response.data['pages'].keys.first
         revisions = response.data['pages'][page_id]['revisions']
         first_revision = revisions[0]
-        content = JSON.parse(first_revision['slots']['main']['*'])
-        return content
+        content = first_revision['slots']['main']['*']
+        begin
+          parsed_content = JSON.parse(content)
+        rescue JSON::ParserError => e
+          puts "Error parsing JSON content: #{e.message}"
+          puts "Content: #{content}"
+          raise e
+        end
+        return parsed_content
       end
+
+      def self.count_claims(content)
+        claims = content['claims']
+        # counting the number of elements inside the arrays in claims
+        if claims
+          claims_lengths = claims.map { |key, value| value.length }
+          total_length = claims_lengths.reduce(0) { |sum, length| sum + length }
+          return total_length
+        else
+          return 0
+        end
+      end
+
+      def self.count_references_recursive(content)
+        references_count = 0
+        if content.is_a?(Hash)
+          content.each do |key, value|
+            if key == 'references' && value.is_a?(Array)
+              references_count += value.length
+            elsif value.is_a?(Array) || value.is_a?(Hash)
+              references_count += count_references_recursive(value)
+            end
+          end
+        elsif content.is_a?(Array)
+          content.each do |item|
+            references_count += count_references_recursive(item)
+          end
+        end
+        return references_count
+      end      
     end
   end
 end
+
+revision_id = 1596238100
+content = Wikidata::Diff::Analyzer.get_revision_content(revision_id)
+claim_count = Wikidata::Diff::Analyzer.count_claims(content)
+references_count = Wikidata::Diff::Analyzer.count_references_recursive(content)
+puts "Claim count: #{claim_count}"
+puts "References count: #{references_count}"
--- spec/wikidata/diff/analyzer_spec.rb
+++ spec/wikidata/diff/analyzer_spec.rb
@@ -4,11 +4,11 @@
 require './lib/wikidata/diff/analyzer'
 require 'rspec'
 
-# It is a testcase testing
+# testcase testing get_revision_content
 # https://www.wikidata.org/w/api.php?action=query&prop=revisions&revids=123456&rvslots=main&rvprop=content&format=json
 # only works with a valid revision id
 # only works having one revision object
-describe Wikidata::Diff::Analyzer do
+RSpec.describe Wikidata::Diff::Analyzer do
   describe '.get_revision_content' do
     it 'returns the content of a revision' do
       content = Wikidata::Diff::Analyzer.get_revision_content(123456)
@@ -26,3 +26,136 @@ describe Wikidata::Diff::Analyzer do
     end
   end
 end
+
+# testcases for claim count
+RSpec.describe Wikidata::Diff::Analyzer do
+  describe '.count_claims' do
+    it 'returns the number of claims in the revision content' do
+      revision_id = 1234567890
+      api_url = 'https://www.wikidata.org/w/api.php'
+    
+      client = instance_double(MediawikiApi::Client)
+      response = instance_double(MediawikiApi::Response)
+      allow(MediawikiApi::Client).to receive(:new).with(api_url).and_return(client)
+      allow(client).to receive(:action).with(
+        'query',
+        prop: 'revisions',
+        revids: revision_id,
+        rvslots: 'main',
+        rvprop: 'content',
+        format: 'json'
+      ).and_return(response)
+    
+      page_id = '123'
+      revisions = [
+        {
+          'slots' => {
+            'main' => {
+                '*' => '{"claims":{"P31":[{"mainsnak":{"snaktype":"value","property":"P31","hash":"ad7d38a03cdd40cdc373de0dc4e7b7fcbccb31d9","datavalue":{"value":{"entity-type":"item","numeric-id":5,"id":"Q5"},"type":"wikibase-entityid"}},"type":"statement","id":"Q111269579$480c779b-4c0e-f9a8-f670-a92ecc122c22","rank":"normal"}]}}'
+            }
+          }
+        }
+      ]
+      allow(response).to receive(:data).and_return({
+        'pages' => {
+          page_id => {
+            'revisions' => revisions
+          }
+        }
+      })
+    
+      content = Wikidata::Diff::Analyzer.get_revision_content(revision_id)
+      expect(Wikidata::Diff::Analyzer.count_claims(content)).to eq(1)
+    end
+
+    it 'returns 0 if the revision content has no claims' do
+      content = {
+        'foo' => 'bar'
+      }
+      expect(Wikidata::Diff::Analyzer.count_claims(content)).to eq(0)
+    end
+  end
+end
+
+# test cases for count_references_recursive
+RSpec.describe '.count_references' do
+  it 'returns the number of references in the revision content' do
+    revision_id = 1234567890
+    api_url = 'https://www.wikidata.org/w/api.php'
+
+    client = instance_double(MediawikiApi::Client)
+    response = instance_double(MediawikiApi::Response)
+    allow(MediawikiApi::Client).to receive(:new).with(api_url).and_return(client)
+    allow(client).to receive(:action).with(
+      'query',
+      prop: 'revisions',
+      revids: revision_id,
+      rvslots: 'main',
+      rvprop: 'content',
+      format: 'json'
+    ).and_return(response)
+
+    page_id = '123'
+    revisions = [
+      {
+        'slots' => {
+          'main' => {
+            '*' => '{"claims":{"P31":[{"mainsnak":{"snaktype":"value","property":"P31","hash":"ad7d38a03cdd40cdc373de0dc4e7b7fcbccb31d9","datavalue":{"value":{"entity-type":"item","numeric-id":5,"id":"Q5"},"type":"wikibase-entityid"}},"type":"statement","id":"Q111269579$480c779b-4c0e-f9a8-f670-a92ecc122c22","rank":"normal","references":[{"hash":"d8d7d6d5d4d3d2d1d0","snaks":{"P248":[{"snaktype":"value","property":"P248","hash":"a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0","datavalue":{"value":{"entity-type":"item","numeric-id":36906466,"id":"Q36906466"},"type":"wikibase-entityid"}}]},"snaks-order":["P248"]}]}]}}'
+          }
+        }
+      }
+    ]
+    allow(response).to receive(:data).and_return({
+      'pages' => {
+        page_id => {
+          'revisions' => revisions
+        }
+      }
+    })
+
+    content = Wikidata::Diff::Analyzer.get_revision_content(revision_id)
+    expect(Wikidata::Diff::Analyzer.count_references_recursive(content)).to eq(1)
+  end
+end
+
+# test cases for count_references_recursive when references count is zero
+RSpec.describe '.count_references' do
+  it 'returns the number of references in the revision content' do
+    revision_id = 1234567890
+    api_url = 'https://www.wikidata.org/w/api.php'
+
+    client = instance_double(MediawikiApi::Client)
+    response = instance_double(MediawikiApi::Response)
+    allow(MediawikiApi::Client).to receive(:new).with(api_url).and_return(client)
+    allow(client).to receive(:action).with(
+      'query',
+      prop: 'revisions',
+      revids: revision_id,
+      rvslots: 'main',
+      rvprop: 'content',
+      format: 'json'
+    ).and_return(response)
+
+    page_id = '123'
+    revisions = [
+      {
+        'slots' => {
+          'main' => {
+            '*' => '{"claims":{}}'
+          }
+        }
+      }
+    ]
+    allow(response).to receive(:data).and_return({
+      'pages' => {
+        page_id => {
+          'revisions' => revisions
+        }
+      }
+    })
+
+    content = Wikidata::Diff::Analyzer.get_revision_content(revision_id)
+    expect(Wikidata::Diff::Analyzer.count_references_recursive(content)).to eq(0)
+  end
+end
+
