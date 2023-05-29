026dbab0a8af2fa13c0f10f766c90c086a9b177a
Sulagna Saha <saha23s@mtholyoke.edu>
2023-05-29 10:01:57 +0600

    initial idea based on revid

--- lib/wikidata/diff/analyzer.rb
+++ lib/wikidata/diff/analyzer.rb
@@ -6,7 +6,36 @@ module Wikidata
   module Diff
     module Analyzer
       class Error < StandardError; end
-      # Your code goes here...
+      def get_parent_revision_id(current_revision_id):
+        # API endpoint URL
+        url = "https://www.wikidata.org/w/api.php"
+
+        # Parameters for the API request
+        params = {
+            "action": "query",
+            "prop": "revisions",
+            "revids": current_revision_id,
+            "rvdir": "older",
+            "rvlimit": "2",
+            "rvprop": "ids",
+            "format": "json"
+        }
+
+        # Send the API request
+        response = requests.get(url, params=params)
+        response_json = response.json()
+
+        # Extract parent revision ID from the response
+        pages = response_json["query"]["pages"]
+        page_id = list(pages.keys())[0]
+        revisions = pages[page_id]["revisions"]
+
+        # Check if the current revision has a parent revision
+        if len(revisions) > 1:
+            parent_revision_id = revisions[1]["revid"]
+            return parent_revision_id
+        return None
+      end
     end
   end
 end
