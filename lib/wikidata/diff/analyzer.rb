559b1d1b1ba0e780694a18f69379d35f85b60e74
Sulagna Saha <saha23s@mtholyoke.edu>
2023-05-29 10:22:31 +0600

    testing to find a revision id having parent id

--- lib/wikidata/diff/analyzer.rb
+++ lib/wikidata/diff/analyzer.rb
@@ -1,41 +1,46 @@
 # frozen_string_literal: true
 
 require_relative "analyzer/version"
+require 'mediawiki_api'
 
 module Wikidata
   module Diff
     module Analyzer
       class Error < StandardError; end
-      def get_parent_revision_id(current_revision_id):
-        # API endpoint URL
-        url = "https://www.wikidata.org/w/api.php"
 
-        # Parameters for the API request
+      def self.get_parent_revision_id(current_revision_id)
+        client = MediawikiApi::Client.new('https://www.wikidata.org/w/api.php')
         params = {
-            "action": "query",
-            "prop": "revisions",
-            "revids": current_revision_id,
-            "rvdir": "older",
-            "rvlimit": "2",
-            "rvprop": "ids",
-            "format": "json"
+          action: 'query',
+          prop: 'revisions',
+          titles: 'Data:Main_Page',
+          rvdir: 'older',
+          rvlimit: 2,
+          rvprop: 'ids',
+          format: 'json'
         }
+        response = client.query(params)
+        pages = response.data['pages']
+        page_id = pages.keys[0]
+        revisions = pages[page_id]['revisions']
 
-        # Send the API request
-        response = requests.get(url, params=params)
-        response_json = response.json()
+        if revisions && revisions.length > 1
+          parent_revision_id = revisions[1]['revid']
+          return parent_revision_id
+        end
 
-        # Extract parent revision ID from the response
-        pages = response_json["query"]["pages"]
-        page_id = list(pages.keys())[0]
-        revisions = pages[page_id]["revisions"]
-
-        # Check if the current revision has a parent revision
-        if len(revisions) > 1:
-            parent_revision_id = revisions[1]["revid"]
-            return parent_revision_id
-        return None
+        nil
       end
     end
   end
+end
+
+# Example usage
+current_revision_id = '1596238176'
+parent_revision_id = Wikidata::Diff::Analyzer.get_parent_revision_id(current_revision_id)
+
+if parent_revision_id
+  puts "Parent Revision ID: #{parent_revision_id}"
+else
+  puts 'No parent revision found.'
 end
